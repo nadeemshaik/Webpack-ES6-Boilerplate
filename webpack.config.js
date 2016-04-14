16f7828b9b4abd65bd4cbbdd0cd342bdff7cb4d8
Nadeem Shaik <nadeemskv@gmail.com>
2016-04-14 16:22:54 +0530

    ES6 Babel - Webpack Boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: __dirname + '/dist/js',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js'],
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+  },
+};
